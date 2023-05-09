2d0936b916d8012bb7e227a223148f53faee4dde
pizzapup <57334884+pizzapup@users.noreply.github.com>
2023-05-08 23:44:43 -0400

    editing accessories

--- src/components/DogForm/DogCard.js
+++ src/components/DogForm/DogCard.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import Preview from "./Preview";
 import { Level, H } from "react-accessible-headings";
-import { useState } from "react";
 import { ref, update } from "firebase/database";
 import { database as db } from "../../firebase/firebaseConfig";
+import { useState } from "react";
+import { AccessoryList } from "../../assets/custom-dog/Accessories /Accessories";
 
-export default function DogCard({ data, gridColSize }) {
+export default function DogCard({ data }) {
   const postKey = data.id;
+  const [clicked, setClicked] = useState(false);
+  const [accessory, setAccessory] = useState("");
   function archiveMove() {
     console.log(data);
     const updates = {};
@@ -23,33 +26,88 @@ export default function DogCard({ data, gridColSize }) {
     archiveMove();
     archiveRemove();
   }
+  const handleChange = (e) => {
+    setAccessory(e.target.value);
+    console.log("TEST", e.target.value);
+
+    const updates = {};
+    updates["posts/" + data.id] = { ...data, accessory: accessory };
+    return update(ref(db), updates);
+  };
   const dogName = <span className="dog-name">{data.name}</span>;
   return (
     <>
       <li key={data.id} className="dog-card">
         <div className="dog-card-media">
-          <Preview values={data} styles={{ borderRadius: "5px" }} />
+          <Preview
+            accessory={accessory}
+            values={data}
+            styles={{ borderRadius: "5px" }}
+          >
+            <div className="dog-card-media-img"></div>
+          </Preview>
+
+          <Link to={`/doggallery/${postKey}`} className="dog-card-link">
+            <div className="dog-card-link-text">
+              <div>
+                Learn more about <p>{dogName}</p>
+              </div>
+            </div>
+          </Link>
         </div>
 
         <div className="dog-card-text">
           <Level>
             <H>{dogName}</H>
           </Level>
+          <div className="dog-card-text-btns">
+            <button onClick={archivePost}>Send home</button>
+            <button onClick={() => setClicked(!clicked)}>
+              Add accessories
+            </button>
+          </div>
         </div>
-
-        <div>
-          {/* <Link to={`/doggallery/${postKey}`} className="dog-card-link"> */}
-          <Link to={`/doggallery/${postKey}`} className="dog-card-links">
-            <div className="dog-card-links-text">
-              {/* <div className="dog-card-link-text"> */}
-              <div>
-                Learn more about <p>{dogName}</p>
-              </div>
-              <button onClick={archivePost}>Send {dogName} home</button>
-            </div>
-          </Link>
+        <div
+          className={`dog-card-text-accessories ${
+            clicked && "dog-card-text-accessories-open"
+          }`}
+        >
+          <fieldset>
+            <legend>Accessories</legend>
+            <select
+              className="dog-card-text-accessories-list"
+              onChange={handleChange}
+              name="accessory"
+            >
+              <option disabled selected value>
+                --select--
+              </option>
+              {AccessoryList.map((item, i) => {
+                return (
+                  <option
+                    value={item.name}
+                    key={`${item}-${i}`}
+                    onChange={handleChange}
+                  >
+                    {item.name}
+                  </option>
+                );
+              })}
+            </select>
+          </fieldset>
         </div>
       </li>
     </>
   );
+}
+{
+  /* <div>
+          <Link to={`/doggallery/${postKey}`} className="dog-card-link">
+            <div className="dog-card-link-text">
+              <div>
+                Learn more about <p>{dogName}</p>
+              </div>
+            </div>
+          </Link>
+        </div>  */
 }
--- src/components/DogForm/Preview.js
+++ src/components/DogForm/Preview.js
@@ -1,3 +1,4 @@
+import Accessory from "../../assets/custom-dog/Accessories /Accessories";
 import Body from "../../assets/custom-dog/Body";
 import Eyes from "../../assets/custom-dog/Eyes";
 import Snout from "../../assets/custom-dog/Snout";
@@ -9,6 +10,7 @@ export default function Preview({ values, children, ...props }) {
         <Body body={values.body} bodyColor={values.bodyColor} />
         <Eyes eyes={values.eyes} eyeColor={values.eyeColor} />
         <Snout mouth={values.mouth} type={values.nose} />
+        {props.accessory && <Accessory accessory={props.accessory} />}
       </div>
       {children}
     </>
