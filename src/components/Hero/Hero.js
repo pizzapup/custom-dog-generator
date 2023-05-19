6c190c3543e2ab9d944559cf67f6abeed5739620
pizzapup <57334884+pizzapup@users.noreply.github.com>
2023-05-19 11:10:41 -0400

    fixed hero responsiveness

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,43 +7,49 @@ import {Slideshow} from "../Slideshow/Slideshow";
 export default function Hero() {
   return (
     <>
+      <div className="hero-bg">
+        <DogPreview />
+        <DogPreview />
+        <DogPreview />
+        <DogPreview />
+        <DogPreview />
+        <DogPreview />
+        <DogPreview />
+        <DogPreview />
+        <DogPreview />
+        <DogPreview />
+        <DogPreview />
+        <DogPreview />
+        <DogPreview />
+      </div>
       <div className="hero">
-        <div className="hero-prev">
-          <DogPreview />
-          <DogPreview />
-          <DogPreview />
-          <DogPreview />
-          <DogPreview />
-          <DogPreview />
-          <DogPreview />
-          <DogPreview />
-          <DogPreview />
-        </div>
         <div className="hero-inner">
-          <h1>&#10024; Custom Dog Generator &#10024;</h1>
-
-          <div>
-            Create your own custom dog illustration using the Pizzapup Custom
-            Dog Generator. <br />
-            Check out the current dogs having a great time on display in the{" "}
-            <Link to="/doggallery" className="hero-link-inText">
-              Gallery
-            </Link>
-            .{/* <p> Some of them have fancy shoes 👠🥾</p> */}
-          </div>
-          <div className="sparkle-wrapper">
-            <h2>Ready to design your own dog illustration? </h2>
-            <div className="sparkles-btn">
-              <Sparkles></Sparkles>
-              <Link to="/createdog" className="hero-btn">
-                Let's do this
+          <div className="hero-text">
+            <h1>&#10024; Custom Dog Generator &#10024;</h1>
+            <div>
+              Create your own custom dog illustration using the Pizzapup Custom
+              Dog Generator. <br />
+              Check out the current dogs having a great time on display in the{" "}
+              <Link to="/doggallery" className="hero-link-inText">
+                Gallery
               </Link>
+              .{/* <p> Some of them have fancy shoes 👠🥾</p> */}
+            </div>
+            <div className="sparkle-wrapper">
+              <h2>Ready to design your own dog illustration? </h2>
+              <div className="sparkles-btn">
+                <Sparkles className="hero-btn">
+                  <Link to="/createdog" className="hero-btn hero-btn-btn">
+                    Let's do this
+                  </Link>
+                </Sparkles>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="hero-preview">
-        <Slideshow />
+        <div className="hero-preview">
+          <Slideshow />
+        </div>
       </div>
     </>
   );
